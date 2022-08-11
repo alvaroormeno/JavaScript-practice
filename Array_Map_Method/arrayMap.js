190dea46270c207e960769083dac428c3b94eb2e
alvaroormeno <alvaro@ormeno.org>
2022-08-11 12:21:41 -0400

    first commit for Array Map Method Practice

---
+++ Array_Map_Method/arrayMap.js
@@ -0,0 +1,10 @@
+let people = [
+  {name: 'John', age:41, job:'Developer'},
+  {name: 'Jose', age:31, job:'Teacher'},
+  {name: 'Alvaro', age:31, job:'Nurse'},
+  {name: 'Maurico', age:47, job:'Police'},
+  {name: 'Paolo', age:11, job:'Author'},
+  {name: 'Salvador', age:21, job:'Artist'},
+  {name: 'Gianmarco', age:56, job:'Doctor'},
+
+]
